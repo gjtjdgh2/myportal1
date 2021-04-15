d58953afab71c4491e0b29b699456fe30bbe15ac
gjtjdgh <gjtjdgh2@naver.com>
2021-04-15 14:59:00 +0900

    방명록 쓰기

--- myportal1/src/main/java/com/bitacademy/myportal1/contoller/GuestBookContoller.java
+++ myportal1/src/main/java/com/bitacademy/myportal1/contoller/GuestBookContoller.java
@@ -4,7 +4,10 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.bitacademy.myportal1.Vo.GuestBookVo;
@@ -18,9 +21,23 @@ public class GuestBookContoller {
 	@ResponseBody
 	@RequestMapping({"","/","/list"})
 	// ->/guestbook ,/guestbook/ ,guestbook/list
-	public String list() {
-		List<GuestBookVo> list =guestBookServiceImpl.getlist();
-		return list.toString();
+	public String list(Model model) {
+		List<GuestBookVo> list = guestBookServiceImpl.getlist();
+		//데이터를 모델에 추가
+		model.addAttribute("list",list);
+		return "guestbook/list";
 	}
 
+//	게시물 작성 메서드
+	@RequestMapping(value="/write",
+			method=RequestMethod.POST)
+	public String write(@ModelAttribute GuestBookVo vo) {
+		System.out.println("VO:" + vo);
+		boolean success = guestBookServiceImpl.writeMessage(vo);
+		System.out.println("Write Result:" + success);
+
+		//	리스트 페이지로 리다이렉트
+		return "redirect:/guestbook";
+	}
+	
 }
--- myportal1/src/main/java/com/bitacademy/myportal1/repository/GuestBookDaoImpl.java
+++ myportal1/src/main/java/com/bitacademy/myportal1/repository/GuestBookDaoImpl.java
@@ -4,27 +4,29 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.bitacademy.myportal1.Vo.GuestBookVo;
 
 @Repository //이름을 명시 하지 않으면 클래스 이름을 기반으로 자동 명명  guestBookDaoImpl 이됨
 public class GuestBookDaoImpl implements GuestBookDao {
+	//데이터 소스 연결
+	@Autowired
+	SqlSession sqlSession;
 
 	@Override
 	public List<GuestBookVo> selectAll() {
-		//가상 데이터
-		List<GuestBookVo> list = new ArrayList<>();
-		list.add(new GuestBookVo(1l,"홍길동","1234","왔다감",new Date()));
-		list.add(new GuestBookVo(2l,"전우치","pass","저두",new Date()));
-		list.add(new GuestBookVo(3l,"길동","test","반갑다",new Date()));
+		List<GuestBookVo> list = sqlSession.selectList("guestbook.selectAll");
+		System.out.println("방명록:" + list);
 		return list;
 	}
 
 	@Override
 	public int insert(GuestBookVo vo) {
-		// TODO Auto-generated method stub
-		return 0;
+		int insertedCount =sqlSession.insert("guestbook.insert",vo);
+		return insertedCount;
 	}
 
 	@Override
